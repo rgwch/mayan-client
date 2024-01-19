64c7d7dc63aec5d02939b43221d7ea25b0d12283
gerry <weirich@elexis.ch>
2024-01-19 16:15:35 +0100

    page loading

---
+++ src/lib/widgets/Dial.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+</script>
+
+<div class="flex flex-row">
+    <button>&lt;</button>
+    <input type="number" />
+    <button>&gt;</button>
+</div>
