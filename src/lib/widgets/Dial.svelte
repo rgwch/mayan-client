0faf4130777feda5fcb75389d9f1f0429b48ca86
gerry <weirich@elexis.ch>
2024-01-20 11:30:46 +0100

    make dial invisible when not needed

--- src/lib/widgets/Dial.svelte
+++ src/lib/widgets/Dial.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
     export let current: number;
-    export let hasNext: boolean=false
-    export let hasPrev:boolean=false
+    export let hasNext: boolean = false;
+    export let hasPrev: boolean = false;
     const dispatch = createEventDispatcher();
     function change(nr: number) {
         current += nr;
@@ -10,13 +10,15 @@
     }
 </script>
 
-<div class="flex flex-row items-end relative bottom-1">
+<div
+    class="flex flex-row items-end relative bottom-1"
+    class:invisible={hasPrev == false && hasNext == false}>
     <button
         class="border border-blue-800 bottom-2 px-1 mx-2 hover:bg-blue-200 h-5 text-sm"
         class:invisible={!hasPrev}
         disabled={!hasPrev}
         on:click={() => change(-1)}>&lt;</button>
-    <div class="mt-1">{current}</div> 
+    <div class="mt-1">{current}</div>
     <button
         class="border border-blue-800 px-1 mx-2 hover:bg-blue-200 h-5 text-sm"
         class:invisible={!hasNext}
