c7f7a74b63d6dba224142c1adb7575765f7a199c
gerry <weirich@elexis.ch>
2024-01-20 12:25:06 +0100

    design

--- src/lib/widgets/Dial.svelte
+++ src/lib/widgets/Dial.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
+    import Fa from "svelte-fa";
+    import {
+        faChevronLeft,
+        faChevronRight,
+    } from "@fortawesome/free-solid-svg-icons";
     export let current: number;
     export let hasNext: boolean = false;
     export let hasPrev: boolean = false;
@@ -14,14 +19,14 @@
     class="flex flex-row items-end relative bottom-1"
     class:invisible={hasPrev == false && hasNext == false}>
     <button
-        class="border border-blue-800 bottom-2 px-1 mx-2 hover:bg-blue-200 h-5 text-sm"
+        class="pb-1 px-1 mx-2 hover:bg-blue-200 h-5 text-sm"
         class:invisible={!hasPrev}
         disabled={!hasPrev}
-        on:click={() => change(-1)}>&lt;</button>
+        on:click={() => change(-1)}><Fa icon={faChevronLeft}></Fa></button>
     <div class="mt-1">{current}</div>
     <button
-        class="border border-blue-800 px-1 mx-2 hover:bg-blue-200 h-5 text-sm"
+        class="px-1 pb-1 mx-2 hover:bg-blue-200 h-5 text-sm"
         class:invisible={!hasNext}
         disabled={!hasNext}
-        on:click={() => change(1)}>&gt;</button>
+        on:click={() => change(1)}><Fa icon={faChevronRight}></Fa></button>
 </div>
