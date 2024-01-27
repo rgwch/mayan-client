68c4465c9476a83324eb23347ee2c8b6cd18bc3e
rgwch <weirich@elexis.ch>
2024-01-27 08:27:27 +0100

    simplified left panel

--- src/lib/Cabinets.svelte
+++ src/lib/Cabinets.svelte
@@ -18,6 +18,7 @@
   import { Tree } from "./model/tree";
   import { CabinetTreeLoader } from "./model/treeloader";
   import Treeview from "./widgets/Treeview.svelte";
+    import GlobalSearches from "./widgets/GlobalSearches.svelte";
   const dispatch = createEventDispatcher();
   let cabinetsOpen = window.screen.availWidth > 767 ? true : false;
   let globalsOpen = window.screen.availWidth > 767 ? true : false;
@@ -67,57 +68,22 @@
 {/if}
 
 <!-- collections not related to a single cabinet -->
-<h1 class="text-xl font-bold mt-4 pt-2 border-t-2 border-blue-200">
-  <a href="#/" on:click={() => (globalsOpen = !globalsOpen)}>
-    {$_("all")}
-  </a>
-</h1>
-{#if globalsOpen}
-  <div transition:slide={{ duration: 200 }}>
-    <ul>
-      <li>
-        <!-- Favorites -->
-        <a
-          href="#/"
-          on:click={() =>
-            dispatch("selected", { id: -3, full_path: $_("favorites") })}
-          >{$_("favorites")}</a>
-      </li>
-      <li>
-        <!-- last created -->
-        <a
-          href="#/"
-          on:click={() =>
-            dispatch("selected", { id: -1, full_path: $_("lastcreated") })}
-          >{$_("lastcreated")}</a>
-      </li>
-      <li>
-        <!-- last accessed -->
-        <a
-          href="#/"
-          on:click={() =>
-            dispatch("selected", { id: -2, full_path: $_("lastaccessed") })}
-          >{$_("lastaccessed")}</a>
-      </li>
-      <li>
-        <!-- matching a search term -->
-        <Search
-          on:search={(event) =>
-            dispatch("selected", { id: -5, full_path: event.detail })}></Search>
-      </li>
-    </ul>
-    <!-- having a tag -->
-    <div class="mt-3 w-32">
-      <Tags on:selected></Tags>
+<div class="mt-4 pt-2 border-t-2 border-blue-200">
+  <Collapse bind:open={globalsOpen}>
+    <div slot="header">
+      <span class="text-xl">{$_("all")}</span>
     </div>
-  </div>
-{/if}
+    <div slot="body">
+      <GlobalSearches></GlobalSearches>
+    </div>
+  </Collapse>
+</div>
 <!-- Create/Upload documents -->
 <div>
   <div class="mt-4 pt-2 border-t-2 border-blue-200">
     <Collapse>
       <div slot="header">
-        <span class="text-xl font-bold">{$_("create")}</span>
+        <span class="text-xl">{$_("create")}</span>
       </div>
       <div slot="body">
         <Uploader></Uploader>
